60e49dcbb8cd42897dc328e207af0a72ac153840
Andrew MacIntyre <andrewmac@gmail.com>
2012-01-17 18:24:31 -0400

    v0.9

---
+++ README.txt
@@ -0,0 +1,55 @@
+
+HDMI-Controller v0.9
+
+Copyright 2012, Andrew Mac
+  http://www.andrewmac.ca
+Licensed under GPL v3.
+  See LICENSE.txt
+
+------------------------------
+ SETUP
+------------------------------
+
+For the controller to function you need to adjust a few values in hdmi_controller.cfg:
+
+    screen = LVDS1
+    screen_resolution = 1440x900 @ 60hz
+    hdmi = HDMI1
+    pulse_default_sink = alsa_output.pci-0000_00_1b.0.analog-stereo
+    hdmi_audio_device = hw:0,3
+
+Open a terminal and run 'xrandr' and it will show you a list, for instance:
+
+    LVDS1 connected 1440x900+0+0 (normal left inverted right x axis y axis) 303mm x 190mm
+       1440x900       59.9*+   59.9  
+       1360x768       59.8     60.0  
+       1152x864       60.0  
+       1024x768       60.0  
+       800x600        60.3     56.2  
+       640x480        59.9  
+    HDMI1 disconnected (normal left inverted right x axis y axis)
+
+The resolution with the * next to it is the one you're currently on. So fill in screen, screen_resolution, and hdmi values. I rounded to 60hz, you can put 59.9hz if you wish.
+
+The next part is a little trickier, you need to find your default pulse audio sink. Run the command 'pacmd list-sinks'. This will list all the sinks, in my case there is only one, the top part of the output looks like this:
+
+  * index: 0
+	name: <alsa_output.pci-0000_00_1b.0.analog-stereo>
+	driver: <module-alsa-card.c>
+	flags: HARDWARE HW_MUTE_CTRL HW_VOLUME_CTRL DECIBEL_VOLUME LATENCY DYNAMIC_LATENCY
+	state: RUNNING
+
+So my pulse_default_sink = alsa_output.pci-0000_00_1b.0.analog-stereo
+This defines the sink the the controller will switch back to when you disable HDMI settings.
+
+The last value to fill in, hdmi_audio_device should be simple. Run the command 'aplay -l' and it will give you a list of audio devices. Each device will be formatted as such:
+
+    card 0: Intel [HDA Intel], device 0: ALC262 Analog [ALC262 Analog]
+      Subdevices: 0/1
+      Subdevice #0: subdevice #0
+
+Note the card number 0 and the device number 0. This would be referred to as hw:0,0. If your HDMI audio device is on card 0 and is device 3 like mine you would set hdmi_audio_device = hw:0,3
+
+Now just run the application and when you click 'Apply HDMI Settings' it should clone your computer screen on the HDMI out, and the audio should now be going over that as well. When you click the disable button it will revert back to your original settings.
+
+
---
+++ hdmi_controller.py
@@ -0,0 +1,186 @@
+"""
+ * HDMI-Controller v0.9
+ *
+ * Copyright 2012, Andrew Mac
+ * http://www.andrewmac.ca
+ * Licensed under GPL v3.
+ * See LICENSE.txt
+"""
+
+import pygtk, sys, ConfigParser, commands, logging
+pygtk.require('2.0')
+import gtk
+
+class HDMIController:
+    def saveAndApply(self, widget, data=None):
+        #self.applySettings()
+        self.saveConfig()
+        print "Saved and applied settings."
+    
+    def disableSettings(self, widget, data=None):
+        # disable HDMI
+        self.cmd("xrandr --output " + self.hdmi_name + " --off")
+
+        # set the screen to default settings.
+        mode_name = self.addMode(self.screen_name, self.screen_res)
+        self.displayConfig(self.screen_name, mode = mode_name, scale = "1x1", primary = True)
+
+        # set pulse back to default audio device
+        self.setDefaultSink(self.default_sink)
+
+    def deleteEvent(self, widget, event, data=None):
+        gtk.main_quit()
+        return False
+
+    def parseResolution(self, res_line):
+        # Takes the format '1920x1080 @ 60hz (16:9)'
+        # and returns [1920, 1080, 60]
+        args = res_line.split(" ")
+        res = args[0].split("x")        
+        hz = args[2][:-2]
+        return [int(res[0]), int(res[1]), int(hz)]
+
+    def cmd(self, command):
+        result = commands.getoutput(command)
+        self.log.debug("Executed command '" + command + "' Result: " + result) 
+        return result
+
+    def addMode(self, display, res):
+        # Get mode line.
+        result = self.cmd("cvt " + str(res[0]) + " " + str(res[1]) + " " + str(res[2])).split("\n")
+        mode_line = result[1][9:]
+        mode_name = mode_line.split(" ")[0].replace("\"", "")
+
+        # Delete mode to be safe, create new mode
+        # then add to the display.
+        #self.cmd("xrandr --rmmode " + mode_name)
+        self.cmd("xrandr --newmode " + mode_line)
+        self.cmd("xrandr --addmode " + display + " " + mode_name)
+        return mode_name
+
+    def displayConfig(self, display_name, mode = None, pos = None, primary = False, scale = None):
+        cfg_line = "xrandr --output " + display_name
+        if ( mode ): cfg_line += " --mode " + mode
+        if ( pos ): cfg_line += " --pos " + mode
+        if ( primary ): cfg_line += " --primary"
+        if ( scale ): cfg_line += " --scale " + scale
+
+        return self.cmd(cfg_line)
+
+    def createSink(self, device, sink_name):
+        self.cmd("pacmd load-module module-alsa-sink device=" + device + " sink_name=" + sink_name)
+
+    def setDefaultSink(self, sink):
+        self.cmd("pacmd set-default-sink " + sink)
+
+    def applySettings(self):
+        # Parse selected resolution.
+        model = self.res_combo.get_model()
+        index = self.res_combo.get_active()
+        res_line = model[index][0]
+        res = self.parseResolution(res_line)
+
+        # Setup HDMI
+        mode_name = self.addMode(self.hdmi_name, res)
+        self.displayConfig(self, self.hdmi_name, mode = mode_name, pos = "0x0", primary = self.primary_check.get_active())
+
+        # Scale the screen
+        h_scale = (float)(res[0])/(float)(self.screen_res[0])
+        v_scale = (float)(res[1])/(float)(self.screen_res[1])
+        self.displayConfig(self, self.screen_name, scale = str(h_scale) + "x" + str(v_scale))
+
+        # Turn on HDMI Audio
+        if ( self.audio_check.get_active() ):
+            self.createSink(self.hdmi_audio, "HDMI")
+            self.setDefaultSink("HDMI")
+
+    def loadConfig(self):
+        self.config = ConfigParser.ConfigParser()
+        self.config.read("hdmi_controller.cfg")
+        
+        # load app settings
+        self.hdmi_name = self.config.get("app_settings", "hdmi")
+        self.hdmi_audio = self.config.get("app_settings", "hdmi_audio_device")
+        self.default_sink = self.config.get("app_settings", "pulse_default_sink")
+        self.screen_name = self.config.get("app_settings", "screen")
+        self.screen_res = self.parseResolution(self.config.get("app_settings", "screen_resolution"))
+        self.res_list = self.config.get("app_settings", "hdmi_resolutions").split(",")
+        for res in self.res_list:
+            self.res_combo.append_text(res)
+
+        # load user settings
+        self.res_combo.set_active(self.config.getint("user_settings", "res_index"))
+        self.primary_check.set_active(self.config.getboolean("user_settings", "hdmi_primary"))
+        self.audio_check.set_active(self.config.getboolean("user_settings", "hdmi_audio"))
+
+    def saveConfig(self):
+        index = self.res_combo.get_active()
+        self.config.set("user_settings", "res_index", str(index))
+        self.config.set("user_settings", "hdmi_primary", str(self.primary_check.get_active()))
+        self.config.set("user_settings", "hdmi_audio", str(self.audio_check.get_active()))
+        with open('hdmi_controller.cfg', 'wb') as configfile:
+            self.config.write(configfile)
+
+    def __init__(self):
+        # setup logging.
+        self.log = logging.getLogger('hdmi_controller')
+        hdlr = logging.FileHandler('hdmi_controller.log')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s : %(message)s')
+        hdlr.setFormatter(formatter)
+        self.log.addHandler(hdlr) 
+        self.log.setLevel(logging.DEBUG)
+
+        # Process any args.
+        for arg in sys.argv:
+            if ( arg == "--enable" ):
+                print "Enabled."
+            if ( arg == "--disable" ):
+                print "Disabled."
+
+        # Create a new window
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("HDMI-Out Controller")
+        self.window.connect("delete_event", self.deleteEvent)
+        self.window.set_border_width(20)
+        self.window.set_icon_from_file("hdmi_logo.png")
+        vbox = gtk.VBox(False, 2)
+        self.window.add(vbox)
+
+        # HDMI logo, illegal?
+        logo = gtk.Image()
+        logo.set_from_file("hdmi_logo.png")
+        vbox.pack_start(logo, True, True, 2)
+
+        # Resolution combo box.
+        self.res_combo = gtk.combo_box_new_text()
+        vbox.pack_start(self.res_combo, True, True, 2)
+
+        # HDMI as Primary Display
+        self.primary_check = gtk.CheckButton("Make HDMI Primary Display")
+        vbox.pack_start(self.primary_check, True, True, 2)
+
+        # Audio over HDMI
+        self.audio_check = gtk.CheckButton("Enable Audio over HDMI")
+        vbox.pack_start(self.audio_check, True, True, 2)
+
+        # Button to Apply/Save settings.
+        button = gtk.Button("Apply and Save Settings")
+        button.connect("clicked", self.saveAndApply)
+        vbox.pack_start(button, True, True, 2)
+
+        # Disable any changes we have active.
+        button = gtk.Button("Disable HDMI Settings")
+        button.connect("clicked", self.disableSettings)
+        vbox.pack_start(button, True, True, 2)
+
+        # load config and show window.
+        self.loadConfig()
+        self.window.show_all()
+
+def main():
+    gtk.main()
+    return 0       
+
+if __name__ == "__main__":
+    HDMIController()
+    main()
